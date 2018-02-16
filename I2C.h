d6cee81ee8c4c87ece1147b5dbd5b69573481b83
Earsuit <renyudicn@outlook.com>
2018-02-16 13:23:55 +0800

    Add files via upload

--- I2C.h
+++ I2C.h
@@ -3,6 +3,8 @@
     tested it on other boards but any one uses ATmega48A/PA/88A/PA/168A/PA/328/P
     or Atmel ATmega640/V-1280/V-1281/V-2560/V-2561/V chip should work.
 
+    Please refer to https://longnight975551865.wordpress.com/2018/02/11/write-your-own-i%c2%b2c-library/ for more information.
+
     Feedback and contribution is welcome!
 
     Created by Yudi Ren, Feb 1, 2018.
